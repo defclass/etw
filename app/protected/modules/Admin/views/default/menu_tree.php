e65b39f0df7f3428e78fcf9f246cccd5b91f707d
snowh4r3 <mike05@qq.com>
2014-05-27 12:15:49 +0800

    规范一下局部代码

--- app/protected/modules/Admin/views/default/menu_tree.php
+++ app/protected/modules/Admin/views/default/menu_tree.php
@@ -4,7 +4,7 @@
 	'htmlOptions'=>array('class'=>'tree treeFolder expand'),
 	'items'=>array(
 		array('label'=>'后台用户管理', 'url'=>array('#'),'items'=>array(
-			array('label'=>'后台用户管理', 'url'=>array('/Admin/Admin/admin'), 'linkOptions'=>array('target'=>'navTab','rel'=>'art_manager')),
+			array('label'=>'后台用户管理', 'url'=>array('/Admin/Admin/admin'), 'linkOptions'=>array('target'=>'navTab','rel'=>'admin_manager')),
 		)),
         array('label'=>'文章与新闻', 'url'=>array('#'),'items'=>array(
 			array('label'=>'文章编辑', 'url'=>array('/Admin/Article/admin'), 'linkOptions'=>array('target'=>'navTab','rel'=>'art_manager')),
@@ -12,13 +12,13 @@
 		)),
 
 		array('label'=>'产品与供应商', 'url'=>array('#'),'items'=>array(
-			array('label'=>'品牌管理', 'url'=>array('/Admin/Brand/admin'), 'linkOptions'=>array('target'=>'navTab','rel'=>'Order')),
-			array('label'=>'供应商管理', 'url'=>array('/Admin/Manufacturer/admin'), 'linkOptions'=>array('target'=>'navTab','rel'=>'Order')),
-			array('label'=>'产品分类', 'url'=>array('/Admin/Classify/admin'), 'linkOptions'=>array('target'=>'navTab','rel'=>'Order')),
-			array('label'=>'产品管理', 'url'=>array('/Admin/Product/admin'), 'linkOptions'=>array('target'=>'navTab','rel'=>'Order')),
+			array('label'=>'品牌管理', 'url'=>array('/Admin/Brand/admin'), 'linkOptions'=>array('target'=>'navTab','rel'=>'product')),
+			array('label'=>'供应商管理', 'url'=>array('/Admin/Manufacturer/admin'), 'linkOptions'=>array('target'=>'navTab','rel'=>'product')),
+			array('label'=>'产品分类', 'url'=>array('/Admin/Classify/admin'), 'linkOptions'=>array('target'=>'navTab','rel'=>'product')),
+			array('label'=>'产品管理', 'url'=>array('/Admin/Product/admin'), 'linkOptions'=>array('target'=>'navTab','rel'=>'product')),
 		)),
 		array('label'=>'订单管理', 'url'=>array('#'),'items'=>array(
-			array('label'=>'订单管理', 'url'=>array('/Admin/Order/admin'), 'linkOptions'=>array('target'=>'navTab','rel'=>'PopularizeDetail')),
+			array('label'=>'订单管理', 'url'=>array('/Admin/Order/admin'), 'linkOptions'=>array('target'=>'navTab','rel'=>'order')),
 		)),
         /* array('label'=>'系统设置', 'url'=>array('#'),'items'=>array( */
 		/* 	array('label'=>'操作日志', 'url'=>array('/Admin/OperateLog/admin'), 'linkOptions'=>array('target'=>'navTab','rel'=>'OperateLog')), */
