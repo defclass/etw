2904c7623a6680f497e425846f4b71f62a02bd03
snowh4r3 <mike05@qq.com>
2014-06-04 12:28:10 +0800

    修正一些错误

--- app/protected/models/Order.php
+++ app/protected/models/Order.php
@@ -60,11 +60,11 @@ class Order extends CActiveRecord
 	{
 		return array(
 			'oid' => '订单ID',
-			'company_name' => Yii::t('site','Company Name'),
-            'contact_person' => Yii::t('site','Contact'),
+			'company_name' => '公司名',
+            'contact_person' => '联系人',
 			'email' => 'email',
-			'tel' => Yii::t('site','Telephone'),
-			'inquiry_content' => Yii::t('site','Inquiry Content'),
+			'tel' => '电话',
+			'inquiry_content' => '询价内容',
 		);
 	}
 
