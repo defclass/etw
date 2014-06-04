96bc13c3b92a3e5910f8c73a047df3ba83481fb0
snowh4r3 <mike05@qq.com>
2014-06-04 20:23:08 +0800

    新增邮件订单的模板

---
+++ app/protected/modules/Admin/views/mail/NewOrder.php
@@ -0,0 +1,37 @@
+<html>
+  <head>
+  </head>
+  <body>
+    订单如下：
+    <br>
+    公司名称：<?php echo $order_obj->company_name;?>
+    <br>
+    联系人：<?php echo $order_obj->contact_person;?>
+    <br>
+    email:<?php echo $order_obj->email;?>
+    <br>
+    电话：<?php echo $order_obj->tel;?>
+    <br>
+    咨询的内容:<?php echo $order_obj->inquiry_content;?>
+    <br>
+    订单详细情况:
+    <br>
+    <table>
+      <tr>
+        <td>产品名</td>
+        <td>需求数量</td>
+        <td>期望供应商</td>
+        <td>期望价格</td>
+      </tr>
+      <tr>
+        <td><?php echo $order_detail->model; ?></td>
+        <td><?php echo $order_detail->quantity; ?></td>
+        <td><?php echo $order_detail->manufacturer; ?></td>
+        <td><?php echo $order_detail->price; ?></td>
+      </tr>
+    </table>
+    ------------------------------------------------------
+    <br>
+    您收到这封邮件是因为您注册了本站会员，如要退订此邮件请<a href='#'>点击</a>。
+  </body>
+</html>
