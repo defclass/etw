f242d1bdd7aebbc67a3d929e5a51e71b6b997c71
snowh4r3 <mike05@qq.com>
2014-06-05 22:13:22 +0800

    判断一下，如果company_url为空则不显示http://

--- app/protected/views/manufacturer/index.php
+++ app/protected/views/manufacturer/index.php
@@ -1,24 +1,27 @@
 
 <!--右侧内容不同区域-->
 <div class="list_detail f_left manufacturer_b">
-    <div class="detail_navigation">
-         <p><a href="<?php echo Yii::app()->createUrl('/Site/Index/'); ?>"><?php echo Yii::t('main','Home'); ?></a> » <a href="<?php echo $this->createUrl('/Manufacturer/Index/'); ?>"><?php echo Yii::t('main','Manufacturers'); ?></a> </p>
-    </div>
+  <div class="detail_navigation">
+    <p><a href="<?php echo Yii::app()->createUrl('/Site/Index/'); ?>"><?php echo Yii::t('main','Home'); ?></a> » <a href="<?php echo $this->createUrl('/Manufacturer/Index/'); ?>"><?php echo Yii::t('main','Manufacturers'); ?></a> </p>
+  </div>
   <table class="list manufacturer_tab">
     <tbody>
 
       <?php foreach($brands as $br ){ ?>
-      <tr>
-        <td class="icon">
-           <a href="<?php echo Yii::app()->createUrl('/Manufacturer/BrandDetails/').'/id/'.$br->bid;?>" target="_blank" title="<?php echo $br->brand_name; ?>"><img src="<?php echo Yii::app()->baseUrl.$br->logo; ?>" alt="<?php echo $br->brand_name; ?>" style="max-width:130px;max-height:45px"></a>
-        </td>
-        <td>
-          <em><a href="<?php echo Yii::app()->createUrl('/Manufacturer/BrandDetails/').'/id/'.$br->bid;?>" target="_blank" title="<?php echo $br->brand_name; ?>"><?php echo $br->brand_name; ?></a></em>
-          <br>http://<?php echo $br->company_url; ?>
-          <br>
-         <?php echo substr( $br->comment,0 ,100); ?>
-        </td>
-      </tr>
+        <tr>
+          <td class="icon">
+            <a href="<?php echo Yii::app()->createUrl('/Manufacturer/BrandDetails/').'/id/'.$br->bid;?>" target="_blank" title="<?php echo $br->brand_name; ?>"><img src="<?php echo Yii::app()->baseUrl.$br->logo; ?>" alt="<?php echo $br->brand_name; ?>" style="max-width:130px;max-height:45px"></a>
+          </td>
+          <td>
+            <em><a href="<?php echo Yii::app()->createUrl('/Manufacturer/BrandDetails/').'/id/'.$br->bid;?>" target="_blank" title="<?php echo $br->brand_name; ?>"><?php echo $br->brand_name; ?></a></em>
+            <br>
+            <?php if(!empty($br->company_url)){ ?>
+              http://<?php echo $br->company_url; ?>
+            <?php } ?>
+            <br>
+            <?php echo substr( $br->comment,0 ,100); ?>
+          </td>
+        </tr>
 
       <?php } ?>
     </tbody></table>
