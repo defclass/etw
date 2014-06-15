9658b556916db77ba85f3f5332bdee83d3cbc831
snowh4r3 <mike05@qq.com>
2014-06-15 20:34:57 +0800

    修改关于我们

--- app/protected/views/contactUs/index.php
+++ app/protected/views/contactUs/index.php
@@ -11,13 +11,13 @@
         <br />
         <div style="height:600px;" class="left-line">    
             <h3>BARUM ELECTRONICS LIMITED</h3>                                  
-          <p><?php echo Yii::t('site','Add: Rm. 1005, Block B,Zhonghangbeiyuan Building,Zhonghang Rd.,Futian District,Shenzhen,Guangdong,China');?></p>
-          <p><?php echo Yii::t('site','Tel');?>: +86 755 82566559</p>
-          <p><?php echo Yii::t('site','Fax');?>: +86 755 82566559</p>
-          <p>Skype: eddie198212<a href=""><img src="../../../images/skyple.jpg"></a></p>
+          <p><strong style='font-weight:bold;'>Add:</strong> <?php echo Yii::t('site','Rm. 1005, Block B,Zhonghangbeiyuan Building,Zhonghang Rd.,Futian District,Shenzhen,Guangdong,China');?></p>
+          <p><strong style='font-weight:bold;'><?php echo Yii::t('site','Tel');?>:</strong> +86 755 82566559</p>
+          <p><strong style='font-weight:bold;'><?php echo Yii::t('site','Fax');?>:</strong> +86 755 82566559</p>
+          <p><strong style='font-weight:bold;'>Skype:</strong> eddie198212<a href=""><img src="../../../images/skyple.jpg"></a></p>
+          <p><strong style='font-weight:bold;'>Email:</strong> info@barum-hk.com</p>
           <div><p><a target="_blank" href="http://webatm.alibaba.com/atm_chat.htm?enemberId=kaijiwei"><img border="0" alt="Contact Us" src="http://amos.us.alitalk.alibaba.com/online.aw?v=2&amp;uid=Barum&amp;site=enaliint&amp;s=5"></a>
             </p></div>
-          <p>Email:info@barum-hk.com</p>
           <p>
             <a href="http://map.baidu.com/?newmap=1&l=12&tn=B_NORMAL_MAP&s=con%26from%3Dalamap%26tpl%3Dmapcity%26wd%3D%E4%B8%AD%E5%9B%BD%E5%B9%BF%E4%B8%9C%E7%9C%81%E6%B7%B1%E5%9C%B3%E5%B8%82%E7%A6%8F%E7%94%B0%E5%8C%BA%E4%B8%AD%E8%88%AA%E8%B7%AF%E4%B8%AD%E8%88%AA%E5%8C%97%E8%8B%91%E5%A4%A7%E5%8E%A6%26c%3D340">
               <img width="280" height="120" target="_blank" src="<?php echo Yii::app()->baseUrl; ?>/images/map1.jpg" alt="<?php echo Yii::t('site','View map');?>"><br><?php echo Yii::t('site','View map');?></a>
